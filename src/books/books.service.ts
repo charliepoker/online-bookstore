bc6da8be6980321c0eac63bbf2e98fac9858ea35
charliepoker <iheanachocharlie@gmail.com>
2022-08-11 22:56:57 +0100

    created book service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(@InjectRepository(Book) private repo: Repository<Book>) {}
+
+  createBook(title: string, author: string, description: string) {
+    const book = this.repo.create({ title, author, description });
+
+    return this.repo.save(book);
+  }
+
+  findOne(id: number) {
+    if (!id) {
+      return null;
+    }
+    return this.repo.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  find(title: string) {
+    return this.repo.find({
+      where: {
+        title: title,
+      },
+    });
+  }
+
+  //   async update(id: number) {
+
+  //   }
+}
